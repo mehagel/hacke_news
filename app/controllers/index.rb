e277c55d59e22402cc79c2664765625887ce5ae9
Apprentice <apprentice@devbootcamp.com>
2013-07-24 18:11:52 -0700

    created post, comment, profile page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,3 +6,12 @@ get '/' do
   erb :index
 end
 
+get '/posts/:post_id' do
+  @post = Post.find(params[:post_id])
+  erb :post
+end
+
+get '/users/:user_id' do
+  @user = User.find(params[:user_id])
+  erb :profile
+end
--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -1,7 +1,34 @@
 helpers do
 
-  def find_username(post_id)
+  def find_username_from_post(post_id)
     User.find(Post.find(post_id).user_id).username
+  end
+
+  def find_username_from_comment(comment_id)
+     User.find(Comment.find(comment_id).user_id).username   
+  end
+
+  def get_all_comments(post_id)
+    @comments = Comment.all.find(post_id)
+    # @comments.each do |comment|
+    #   if comment.post_id == post_id
+    #     comment
+    #   end
+    # end
+  end
+
+  def get_posts(user_id)
+    @user_posts = Post.all.find(user_id)
+   #  @posts = Post.all
+   #  @posts.each do |post|
+   #    if post.user_id == user_id
+   #      post
+   #    end
+   # @post = Post.find(@user.id)
+  end
+
+  def get_comments(user_id)
+    @user_comments = Comment.all.find(@user_id)
   end
 
 end
